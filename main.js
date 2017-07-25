5049938b89d3416c071806a18625ff718863af23
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-25 15:41:07 -0400

    changed randomization, added counter for choices

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
   let win = 0;
   let loss = 0;
   let tie = 0;
+  let playerrock = 0;
+  let playerpaper = 0;
+  let playerscissors = 0;
+  let computerrock = 0;
+  let computerpaper = 0;
+  let computerscissors = 0;
   let list = [];
 $(".Playgame").click(function() {
 console.log(list);
@@ -22,7 +28,8 @@ console.log(listr);
 //CANNOT USE === ON NUMBERS
 let me = "";
 let computer = "";
-let pick = (Math.floor(Math.random() *3 ));
+// let pick = (Math.floor(Math.random() *3 ));
+let pick = Math.random();
 let computeranswer = document.getElementById("computeranswer");
 let playeru = document.getElementById("player").value;
 let player = playeru.toLowerCase(); //Forces playeru to be lowercase
@@ -35,6 +42,7 @@ switch (player) {
         document.getElementById("playerpicture").style.backgroundImage = "url('img/rock.png')";
         makecomputergo();
         list.push("rock");
+        playerrock = playerrock + 1;
         break;
     case "paper":
 			  me = "paper";
@@ -43,6 +51,7 @@ switch (player) {
         document.getElementById("playerpicture").style.backgroundImage = "url('img/paper.png')";
         makecomputergo();
         list.push("paper");
+        playerpaper = playerpaper + 1;
         break;
     case "scissors":
 			  me = "scissors";
@@ -51,23 +60,27 @@ switch (player) {
         document.getElementById("playerpicture").style.backgroundImage = "url('img/scissors.png')";
         makecomputergo();
         list.push("scissors");
+        playerscissors = playerscissors + 1;
         break;
     default:
         youpicked.innerHTML= "Please type either rock, paper, or scissors into the box!";
 }
 function makecomputergo(){
-if (pick == "0") {
+if (pick <0.34) {
   computer = "rock";
   computeranswer.innerHTML= "rock";
   document.getElementById("computerpicture").style.backgroundImage = "url('img/rock.png')";
-} else if (pick == "1") {
+  computerrock = computerrock + 1;
+} else if (pick <=0.67) {
 	computer = "paper";
 	computeranswer.innerHTML= "paper";
   document.getElementById("computerpicture").style.backgroundImage = "url('img/paper.png')";
-} else if (pick == "2") {
+  computerpaper = computerpaper + 1;
+} else {
   computer = "scissors";
 	computeranswer.innerHTML= "scissors";
   document.getElementById("computerpicture").style.backgroundImage = "url('img/scissors.png')";
+  computerscissors = computerscissors + 1;
 }}
 document.getElementById("answer");
 if (player == "rock" && computer == "rock") {
@@ -114,7 +127,18 @@ document.getElementById("losses");
 losses.innerHTML= loss;
 document.getElementById("ties");
 ties.innerHTML= tie;
-
+document.getElementById("procks");
+procks.innerHTML= playerrock;
+document.getElementById("ppapers");
+ppapers.innerHTML= playerpaper;
+document.getElementById("pscissors");
+pscissors.innerHTML= playerscissors;
+document.getElementById("crocks");
+crocks.innerHTML= computerrock;
+document.getElementById("cpapers");
+cpapers.innerHTML= computerpaper;
+document.getElementById("cscissors");
+cscissors.innerHTML= computerscissors;
 });
 });
 //$(document).ready(main);

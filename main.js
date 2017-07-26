2930fd65928b9a6b9a5b5e8913c877200f8eddb4
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-26 10:20:46 -0400

    contain background, add pre-parallax

--- main.js
+++ main.js
@@ -147,4 +147,19 @@ cpaperspercentage.innerHTML= Math.round(computerpaper/turns*100);
 cscissors.innerHTML= computerscissors;
 cscissorspercentage.innerHTML= Math.round(computerscissors/turns*100);
 };
+window.requestAnimationFrame = window.requestAnimationFrame
+ || window.mozRequestAnimationFrame
+ || window.webkitRequestAnimationFrame
+ || window.msRequestAnimationFrame
+ || function(f){setTimeout(f, 1000/60)}
+
+var parallax1 = document.getElementById('parallax1')
+
+function parallaxeffect(){
+  var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
+  parallax1.style.backgroundPosition = "0px " + -scrolltop * .5 + 'px'
+}
+window.addEventListener('scroll', function(){ // on page scroll
+  requestAnimationFrame(parallaxeffect) // call parallaxeffect() every screen paint
+}, false)
 });
